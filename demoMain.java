54775d5466c9cf0359706b7afe0137e269ff2acb
Porfus <lucaporfiri@gmail.com>
2024-10-10 11:53:11 +0200

    Porfuss modifica tutto

---
+++ demoMain.java
@@ -0,0 +1,6 @@
+public class demoMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World- Porfuss was here");
+        System.out.println("Questo progetto è una cacata");
+    }
+}
